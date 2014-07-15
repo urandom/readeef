c42b66b92d0359b4bb654de1d350b28350ad1f17
Viktor Kojouharov <viktor@imperia.net>
2014-07-15 10:38:43 +0300

    split the db file

--- db.go
+++ db.go
@@ -8,18 +8,6 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-const (
-	exists_user = `SELECT 1 FROM users WHERE login = ?;`
-
-	get_user    = `SELECT first_name, last_name, email, salt, hash, md5_api FROM users WHERE login = ?;`
-	create_user = `
-INSERT INTO users(first_name, last_name, email, salt, hash, md5_api, login)
-	VALUES(?, ?, ?, ?, ?, ?, ?);`
-	update_user = `
-UPDATE users SET first_name = ?, last_name = ?, email = ?, salt = ?, hash = ?, md5_api = ?
-	WHERE login = ?;`
-)
-
 var (
 	init_sql = []string{`
 PRAGMA foreign_keys = ON;`, `
@@ -108,54 +96,6 @@ func (db *DB) Connect() error {
 	db.DB = dbx
 
 	return db.init()
-}
-
-func (db DB) GetUser(login string) (User, error) {
-	var u User
-	if err := db.Get(&u, get_user, login); err != nil {
-		return User{}, err
-	}
-
-	u.Login = login
-
-	return u, nil
-}
-
-func (db DB) UpdateUser(u User) error {
-	if err := u.Validate(); err != nil {
-		return err
-	}
-
-	tx, err := db.Beginx()
-	if err != nil {
-		return err
-	}
-
-	row := db.QueryRow(exists_user, u.Login)
-	var exists int
-	row.Scan(&exists)
-
-	var stmt *sqlx.Stmt
-
-	if exists == 1 {
-		stmt, err = tx.Preparex(update_user)
-	} else {
-		stmt, err = tx.Preparex(create_user)
-	}
-
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-
-	_, err = stmt.Exec(u.FirstName, u.LastName, u.Email, u.Salt, u.Hash, u.MD5API, u.Login)
-	if err != nil {
-		return err
-	}
-
-	tx.Commit()
-
-	return nil
 }
 
 func (db DB) init() error {
---
+++ db_user.go
@@ -0,0 +1,63 @@
+package readeef
+
+import "github.com/jmoiron/sqlx"
+
+const (
+	exists_user = `SELECT 1 FROM users WHERE login = ?;`
+
+	get_user    = `SELECT first_name, last_name, email, salt, hash, md5_api FROM users WHERE login = ?;`
+	create_user = `
+INSERT INTO users(first_name, last_name, email, salt, hash, md5_api, login)
+	VALUES(?, ?, ?, ?, ?, ?, ?);`
+	update_user = `
+UPDATE users SET first_name = ?, last_name = ?, email = ?, salt = ?, hash = ?, md5_api = ?
+	WHERE login = ?;`
+)
+
+func (db DB) GetUser(login string) (User, error) {
+	var u User
+	if err := db.Get(&u, get_user, login); err != nil {
+		return User{}, err
+	}
+
+	u.Login = login
+
+	return u, nil
+}
+
+func (db DB) UpdateUser(u User) error {
+	if err := u.Validate(); err != nil {
+		return err
+	}
+
+	tx, err := db.Beginx()
+	if err != nil {
+		return err
+	}
+
+	row := db.QueryRow(exists_user, u.Login)
+	var exists int
+	row.Scan(&exists)
+
+	var stmt *sqlx.Stmt
+
+	if exists == 1 {
+		stmt, err = tx.Preparex(update_user)
+	} else {
+		stmt, err = tx.Preparex(create_user)
+	}
+
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(u.FirstName, u.LastName, u.Email, u.Salt, u.Hash, u.MD5API, u.Login)
+	if err != nil {
+		return err
+	}
+
+	tx.Commit()
+
+	return nil
+}
