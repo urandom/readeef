aa51da0c744f60a13e0fe24b5a42b6992d0ce00f
Viktor Kojouharov <developer@sugr.org>
2014-07-28 22:37:40 +0300

    prepare to update the feeds

--- feed_updater.go
+++ feed_updater.go
@@ -1,23 +1,28 @@
 package readeef
 
-import "log"
+import (
+	"log"
+	"time"
+)
 import "net/http"
 
 type FeedUpdater struct {
-	config     Config
-	db         DB
-	feeds      []Feed
-	addFeed    chan Feed
-	removeFeed chan Feed
-	updateFeed chan<- Feed
-	done       chan bool
-	client     *http.Client
-	logger     *log.Logger
+	config      Config
+	db          DB
+	feeds       []Feed
+	addFeed     chan Feed
+	removeFeed  chan Feed
+	updateFeed  chan<- Feed
+	done        chan bool
+	client      *http.Client
+	logger      *log.Logger
+	feedTickers map[string]time.Ticker
 }
 
 func NewFeedUpdater(db DB, c Config, l *log.Logger) FeedUpdater {
 	return FeedUpdater{db: db, config: c, done: make(chan bool), logger: l,
-		client: NewTimeoutClient(c.Timeout.Converted.Connect, c.Timeout.Converted.ReadWrite)}
+		feedTickers: map[string]time.Ticker{},
+		client:      NewTimeoutClient(c.Timeout.Converted.Connect, c.Timeout.Converted.ReadWrite)}
 }
 
 func (fu FeedUpdater) Start() {
@@ -37,12 +42,20 @@ func (fu FeedUpdater) RemoveFeed(f Feed) {
 func (fu FeedUpdater) reactToChanges() {
 	for {
 		select {
-		case <-fu.addFeed:
-		case <-fu.removeFeed:
+		case f := <-fu.addFeed:
+			fu.startUpdatingFeed(f)
+		case f := <-fu.removeFeed:
+			if t, ok := fu.feedTickers[f.Link]; ok {
+				t.Stop()
+				delete(fu.feedTickers, f.Link)
+			}
 		case <-fu.done:
 			break
 		}
 	}
+}
+
+func (fu FeedUpdater) startUpdatingFeed(f Feed) {
 }
 
 func (fu FeedUpdater) scheduleFeeds() {
