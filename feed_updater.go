1057ce0635b7868b2666a5656d964358ca216440
Viktor Kojouharov <developer@sugr.org>
2014-07-28 22:21:21 +0300

    add methods for adding/removing a feed

--- feed_updater.go
+++ feed_updater.go
@@ -7,8 +7,8 @@ type FeedUpdater struct {
 	config     Config
 	db         DB
 	feeds      []Feed
-	addFeed    <-chan Feed
-	removeFeed <-chan Feed
+	addFeed    chan Feed
+	removeFeed chan Feed
 	updateFeed chan<- Feed
 	done       chan bool
 	client     *http.Client
@@ -23,7 +23,15 @@ func NewFeedUpdater(db DB, c Config, l *log.Logger) FeedUpdater {
 func (fu FeedUpdater) Start() {
 	go fu.reactToChanges()
 
-	go fu.getFeeds()
+	go fu.scheduleFeeds()
+}
+
+func (fu FeedUpdater) AddFeed(f Feed) {
+	fu.addFeed <- f
+}
+
+func (fu FeedUpdater) RemoveFeed(f Feed) {
+	fu.removeFeed <- f
 }
 
 func (fu FeedUpdater) reactToChanges() {
@@ -37,7 +45,7 @@ func (fu FeedUpdater) reactToChanges() {
 	}
 }
 
-func (fu FeedUpdater) getFeeds() {
+func (fu FeedUpdater) scheduleFeeds() {
 	feeds, err := fu.db.GetUnsubscribedFeed()
 	if err != nil {
 		fu.logger.Printf("Error fetching unsubscribed feeds: %v\n", err)
