33173c77cfb72d85f0ef11a15a7c5229df38c8c8
Viktor Kojouharov <developer@sugr.org>
2014-08-29 14:04:10 +0300

    show the origin feed when browsing all

--- static/js/content-articles-worker.js
+++ static/js/content-articles-worker.js
@@ -1,9 +1,21 @@
 self.addEventListener('message', function(event) {
     "use strict";
-    var articles = [];
+    var articles = [], current = event.data.current, feeds = event.data.feeds, feedMap;
 
-    for (var i = 0, a; a = event.data[i]; ++i) {
-        a.ShortDescription = a.Description.replace(/<!--.*?-->/g, '').replace(/<\w[^>]*>/g, '').replace(/<\/[^>]*>/g, '').trim().replace(/\s\s+/g, ' ');
+    for (var i = 0, a; a = current.Articles[i]; ++i) {
+        a.ShortDescription = a.Description.replace(/<!--.*?-->/g, '')
+            .replace(/<\w[^>]*>/g, '').replace(/<\/[^>]*>/g, '').trim().replace(/\s\s+/g, ' ');
+
+        if (current.Id == "__all__") {
+            if (!feedMap) {
+                feedMap = {};
+                for (var j = 0, f; f = feeds[j]; ++j) {
+                    feedMap[f.Id] = f;
+                }
+            }
+
+            a.FeedOrigin = feedMap[a.FeedId].Title;
+        }
         articles.push(a);
     }
 
