457ee3b52f12de07a851b791e2c15c7fb249cc53
Viktor Kojouharov <developer@sugr.org>
2014-09-30 21:35:21 +0300

    toggle the article read state button with a delay on new articles

--- static/js/rf-scaffolding.js
+++ static/js/rf-scaffolding.js
@@ -3,6 +3,9 @@
 
     Polymer('rf-scaffolding', {
         display: "feed",
+        articleRead: false,
+        readStateJob: null,
+        pathObserver: null,
 
         ready: function() {
             var drawerPanel = this.$['drawer-panel'];
@@ -17,6 +20,37 @@
         },
 
         articleChanged: function() {
+            var processArticleState = (function processArticleState(newArticle) {
+                if (this.readStateJob) {
+                    this.readStateJob.stop();
+                }
+
+                if (this.article.Read && !newArticle && this.articleIsRead) {
+                    this.readStateJob = Polymer.job.call(this, this.readStateJob, function() {
+                        this.articleRead = true;
+                    }, 1000);
+                } else {
+                    this.articleRead = this.article.Read;
+                }
+
+                if (newArticle && !this.article.Read) {
+                    this.articleIsRead = true;
+                } else {
+                    this.articleIsRead = false;
+                }
+            }).bind(this);
+
+            if (this.pathObserver) {
+                this.pathObserver.close();
+            }
+
+            if (this.article) {
+                processArticleState(true);
+
+                this.pathObserver = new PathObserver(this, 'article.Read');
+                this.pathObserver.open(function() { processArticleState(false) });
+            }
+
             this.$['drawer-panel'].disableSwipe = !!this.article;
         },
 
