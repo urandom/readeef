6b333cc07e1e099347da1cc706c0a165c9d794dd
Viktor Kojouharov <viktor@imperia.net>
2014-10-24 10:22:26 +0300

    Add debug printout

--- api/fever.go
+++ api/fever.go
@@ -268,6 +268,7 @@ func (con Fever) Handler(c context.Context) http.HandlerFunc {
 			}
 
 			if _, ok := r.Form["links"]; ok {
+				readeef.Debug.Println("Fetching fever links")
 				//offset, _ := strconv.ParseInt(r.FormValue("offset"), 10, 64)
 
 				rng, e := strconv.ParseInt(r.FormValue("range"), 10, 64)
