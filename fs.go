d195a185626ce8fc3b3ad266a89a86dbe649631a
Viktor Kojouharov <viktor@imperia.net>
2015-02-19 10:16:13 +0200

    use an input file to list the external files

--- fs.go
+++ fs.go
@@ -4,7 +4,7 @@ package readeef
 
 import "fmt"
 
-//go:generate webfw-fs -output fs_files.go -package readeef -format -build-tags fs templates/... static/js/... static/images/... static/css/...
+//go:generate webfw-fs -output fs_files.go -package readeef -format -build-tags fs -input file.list
 
 func init() {
 	_, err := addFiles()
