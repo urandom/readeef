8d44b20fa02444ee5b33c459d8a2810bdeaf2b2f
Viktor Kojouharov <developer@sugr.org>
2015-02-27 09:08:03 +0200

    add the sqlite3 repo

---
+++ content/sql/sqlite3/repo.go
@@ -0,0 +1,15 @@
+package sqlite3
+
+import (
+	"github.com/urandom/readeef/content/sql"
+	"github.com/urandom/readeef/db"
+	"github.com/urandom/webfw"
+)
+
+type Repo struct {
+	*sql.Repo
+}
+
+func NewRepo(db *db.DB, logger webfw.Logger) *Repo {
+	return &Repo{Repo: sql.NewRepo(db, logger)}
+}
