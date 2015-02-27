e02aea0ac9295b57a244655b25bfd5d46444d7c0
Viktor Kojouharov <developer@sugr.org>
2015-02-27 09:06:54 +0200

    add the sqlite3 helper

---
+++ db/sqlite3/helper.go
@@ -0,0 +1,47 @@
+package sqlite3
+
+import (
+	"github.com/urandom/readeef/db"
+	"github.com/urandom/readeef/db/base"
+)
+
+type Helper struct {
+	base.Helper
+}
+
+func (h Helper) Init() []string {
+	return initSQL
+}
+
+func init() {
+	helper := &Helper{}
+
+	helper.Set("get_user_feeds", getUserFeeds)
+	helper.Set("get_user_tag_feeds", getUserTagFeeds)
+	helper.Set("get_latest_feed_articles", getLatestFeedArticles)
+
+	db.Register("sqlite3", helper)
+}
+
+const (
+	getUserFeeds = `
+SELECT f.id, f.link, f.title, f.description, f.link, f.hub_link, f.site_link, f.update_error, f.subscribe_error
+FROM feeds f, users_feeds uf
+WHERE f.id = uf.feed_id
+	AND uf.user_login = $1
+ORDER BY f.title COLLATE NOCASE
+`
+	getUserTagFeeds = `
+SELECT f.id, f.link, f.title, f.description, f.link, f.hub_link, f.site_link, f.update_error, f.subscribe_error
+FROM feeds f, users_feeds_tags uft
+WHERE f.id = uft.feed_id
+	AND uft.user_login = $1 AND uft.tag = $2
+ORDER BY f.title COLLATE NOCASE
+`
+
+	getLatestFeedArticles = `
+SELECT a.feed_id, a.id, a.title, a.description, a.link, a.date, a.guid
+FROM articles a
+WHERE a.feed_id = $1 AND a.date > DATE('NOW', '-5 days')
+`
+)
---
+++ db/sqlite3/init.go
@@ -0,0 +1,113 @@
+package sqlite3
+
+import _ "github.com/mattn/go-sqlite3"
+
+var (
+	initSQL = []string{`
+PRAGMA foreign_keys = ON;`, `
+PRAGMA journal_mode = WAL;`, `
+CREATE TABLE IF NOT EXISTS readeef (
+	db_version INTEGER
+)`, `
+CREATE TABLE IF NOT EXISTS users (
+	login TEXT PRIMARY KEY,
+	first_name TEXT,
+	last_name TEXT,
+	email TEXT,
+	admin INTEGER DEFAULT 0,
+	active INTEGER DEFAULT 1,
+	profile_data TEXT,
+	hash_type TEXT,
+	salt TEXT,
+	hash TEXT,
+	md5_api TEXT
+)`, `
+CREATE TABLE IF NOT EXISTS feeds (
+	id INTEGER PRIMARY KEY,
+	link TEXT NOT NULL UNIQUE,
+	title TEXT,
+	description TEXT,
+	hub_link TEXT,
+	site_link TEXT,
+	update_error TEXT,
+	subscribe_error TEXT
+)`, `
+CREATE TABLE IF NOT EXISTS feed_images (
+	id INTEGER PRIMARY KEY,
+	feed_id INTEGER NOT NULL,
+	title TEXT,
+	url TEXT,
+	width INTEGER,
+	height INTEGER,
+
+	FOREIGN KEY(feed_id) REFERENCES feeds(id) ON DELETE CASCADE
+)`, `
+CREATE TABLE IF NOT EXISTS articles (
+	id INTEGER PRIMARY KEY,
+	feed_id INTEGER,
+	link TEXT,
+	guid TEXT,
+	title TEXT,
+	description TEXT,
+	date TIMESTAMP,
+
+	UNIQUE(feed_id, link),
+	UNIQUE(feed_id, guid),
+	FOREIGN KEY(feed_id) REFERENCES feeds(id) ON DELETE CASCADE
+)`, `
+CREATE TABLE IF NOT EXISTS users_feeds (
+	user_login TEXT,
+	feed_id INTEGER,
+
+	PRIMARY KEY(user_login, feed_id),
+	FOREIGN KEY(user_login) REFERENCES users(login) ON DELETE CASCADE,
+	FOREIGN KEY(feed_id) REFERENCES feeds(id) ON DELETE CASCADE
+)`, `
+CREATE TABLE IF NOT EXISTS users_feeds_tags (
+	user_login TEXT,
+	feed_id INTEGER,
+	tag TEXT,
+
+	PRIMARY KEY(user_login, feed_id, tag),
+	FOREIGN KEY(user_login, feed_id) REFERENCES users_feeds(user_login, feed_id) ON DELETE CASCADE
+)`, `
+CREATE TABLE IF NOT EXISTS users_articles_read (
+	user_login TEXT,
+	article_id INTEGER,
+
+	PRIMARY KEY(user_login, article_id),
+	FOREIGN KEY(user_login) REFERENCES users(login) ON DELETE CASCADE,
+	FOREIGN KEY(article_id) REFERENCES articles(id) ON DELETE CASCADE
+)`, `
+CREATE TABLE IF NOT EXISTS users_articles_fav (
+	user_login TEXT,
+	article_id INTEGER,
+
+	PRIMARY KEY(user_login, article_id),
+	FOREIGN KEY(user_login) REFERENCES users(login) ON DELETE CASCADE,
+	FOREIGN KEY(article_id) REFERENCES articles(id) ON DELETE CASCADE
+)`, `
+CREATE TABLE IF NOT EXISTS articles_scores (
+	article_id BIGINT,
+	score  INTEGER,
+	score1 INTEGER,
+	score2 INTEGER,
+	score3 INTEGER,
+	score4 INTEGER,
+	score5 INTEGER,
+
+	PRIMARY KEY(article_id),
+	FOREIGN KEY(article_id) REFERENCES articles(id) ON DELETE CASCADE
+)`, `
+CREATE TABLE IF NOT EXISTS hubbub_subscriptions (
+	feed_id INTEGER,
+	link TEXT,
+	lease_duration INTEGER,
+	verification_time TIMESTAMP,
+	subscription_failure INTEGER DEFAULT 0,
+
+	PRIMARY KEY(feed_id),
+	FOREIGN KEY(feed_id) REFERENCES feeds(id) ON DELETE CASCADE
+)`,
+	}
+)
