dc41dc5e113a08075070de078d28d0154ae36efd
Viktor Kojouharov <viktor@imperia.net>
2015-03-05 11:09:45 +0200

    enable the fs embedding by default

--- fs.go
+++ fs.go
@@ -1,11 +1,11 @@
-// +build fs
+// +build !nofs
 
 package readeef
 
 import "fmt"
 
 //go:generate go run ./cmd/readeef-static-locator/main.go -output file.list
-//go:generate webfw-fs -output fs_files.go -package readeef -format -build-tags fs -input file.list
+//go:generate webfw-fs -output fs_files.go -package readeef -format -build-tags !nofs -input file.list
 
 func init() {
 	_, err := addFiles()
