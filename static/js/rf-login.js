6d21e9405fbe468b34739a01265f7988ad49f934
Viktor Kojouharov <developer@sugr.org>
2015-07-17 20:15:34 +0300

    Add the login js code

--- static/js/rf-login.js
+++ static/js/rf-login.js
@@ -1,29 +1,37 @@
 (function() {
     "use strict";
 
-    Polymer('rf-login', {
-        invalid: false,
+    Polymer({
+        is: 'rf-login',
+        properties: {
+            invalid: {
+                type: Boolean,
+                value: false
+            }
+        },
 
         onKeypress: function(event) {
             var code = event.keyCode || event.charCode, key = event.keyIdentifier;
 
             if (key == 'Enter' || code == 13) {
-                if (event.target === this.$.login) { 
-                    this.$.password.focus();
-                } else if (event.target === this.$.password) {
-                    this.$.submit.asyncFire('tap');
+                if (event.target === this.$.login.$.input) { 
+                    this.$.password.$.input.focus();
+                } else if (event.target === this.$.password.$.input) {
+                    this.$.submit.fire('tap');
                 }
             }
         },
 
         onLogin: function() {
-            this.user = {
+            this.$.login.value = "";
+            this.$.password.value = "";
+
+            var user = {
                 Login: this.$.login.value,
                 MD5API: CryptoJS.MD5(this.$.login.value + ":" + this.$.password.value).toString(CryptoJS.enc.Base64)
             };
 
-            this.$.login.value = "";
-            this.$.password.value = "";
+            Polymer.dom(document).querySelector('rf-router')._setUser(user);
         }
     });
 })();
