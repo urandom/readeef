7fbda998600f073381ef5e22633d949ccf8a3e72
Viktor Kojouharov <developer@sugr.org>
2015-07-19 20:41:21 +0300

    more work on integrating a drawer into the router

--- static/js/rf-login.js
+++ static/js/rf-login.js
@@ -3,11 +3,56 @@
 
     Polymer({
         is: 'rf-login',
+        behaviors: [
+          Polymer.NeonAnimationRunnerBehavior
+        ],
         properties: {
             invalid: {
                 type: Boolean,
                 value: false
+            },
+            animationConfig: {
+                value: function() {
+                    return {
+                        'entry': [{
+                            name: 'slide-down-animation',
+                            node: this.$['login-card']
+                        }, {
+                            name: 'fade-in-animation',
+                            node: this.$['login-card']
+                        }],
+
+                        'exit': [{
+                            name: 'slide-up-animation',
+                            node: this.$['login-card']
+                        }, {
+                            name: 'fade-out-animation',
+                            node: this.$['login-card']
+                        }]
+                    };
+                }
             }
+        },
+
+        attached: function() {
+            this.show();
+        },
+
+        show: function() {
+            this.$['login-card'].style.display = '';
+            this.playAnimation('entry');
+        },
+
+        hide: function() {
+            var self = this;
+
+            this.playAnimation('exit');
+
+            var onFinish = function() {
+                self.$['login-card'].style.display = 'none';
+                self.removeEventListener('neon-animation-finish', onFinish);
+            }
+            this.addEventListener('neon-animation-finish', onFinish);
         },
 
         onKeypress: function(event) {
@@ -31,7 +76,7 @@
                 MD5API: CryptoJS.MD5(this.$.login.value + ":" + this.$.password.value).toString(CryptoJS.enc.Base64)
             };
 
-            Polymer.dom(document).querySelector('rf-router')._setUser(user);
+            Polymer.dom(document).querySelector('rf-router').validateUser(user);
         }
     });
 })();
