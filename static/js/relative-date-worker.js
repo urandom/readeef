99bc216b023966457c254e8c9f4e423fbec69d9a
Viktor Kojouharov <developer@sugr.org>
2015-08-23 19:39:08 +0300

    update the relative date each minute

--- static/js/relative-date-worker.js
+++ static/js/relative-date-worker.js
@@ -2,11 +2,11 @@ importScripts("/dist/moment/min/moment.min.js");
 
 self.addEventListener('message', function(event) {
     "use strict";
-    var dates = {}, current = event.data.current;
+    var dates = {}, articles = event.data.articles;
 
-    for (var i = 0, a; a = current.Articles[i]; ++i) {
+    for (var i = 0, a; a = articles[i]; ++i) {
         dates[a.Id] = moment(a.Date).fromNow();
     }
 
-    self.postMessage({dates: dates, feedId: current.Id});
+    self.postMessage({dates: dates, tagOrId: event.data.tagOrId});
 });
